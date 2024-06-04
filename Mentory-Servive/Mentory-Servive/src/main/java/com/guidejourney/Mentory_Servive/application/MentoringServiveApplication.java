2b84155bf78a41bd41957c9e90fa3649049f8f49
Anthony <alezcanosaavedra@gmail.com>
2024-06-04 14:07:28 -0500

    Commit 0

---
+++ Mentory-Servive/Mentory-Servive/src/main/java/com/guidejourney/Mentory_Servive/application/MentoringServiveApplication.java
@@ -0,0 +1,11 @@
+package com.guidejourney.Mentory_Servive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoryServiveApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MentoryServiveApplication.class, args);
+	}
+}
